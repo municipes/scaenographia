4d3479408f3b9c4ed5ff14bed8e94600f34995ba
Maurizio Cavalletti <maurizio@akabit.it>
2024-04-08 18:24:19 +0200

    fix: don't print empty views

--- modules/bi_taxonomy_custom_controller/src/EventSubscriber/TermPageBuildSubscriber.php
+++ modules/bi_taxonomy_custom_controller/src/EventSubscriber/TermPageBuildSubscriber.php
@@ -8,6 +8,9 @@ use Drupal\Core\Link;
 use Drupal\taxonomy_custom_controller\Event\TaxonomyCustomControllerEvents;
 use Drupal\taxonomy_custom_controller\Event\TermPageBuildEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\views\Views;
+use Drupal\views\ViewExecutable;
+
 
 /**
  * Provides subscriber for term page build.
@@ -92,6 +95,8 @@ class TermPageBuildSubscriber implements EventSubscriberInterface {
           'w-100',
         ];
         $term = \Drupal::routeMatch()->getParameter('taxonomy_term');
+        $view_name = 'taxonomy_term';
+
         # Novità
         $url = Url::fromUserInput('/novita');
         $link = Link::fromTextAndUrl(t('Tutte le novità'), $url)->toRenderable();
@@ -99,18 +104,23 @@ class TermPageBuildSubscriber implements EventSubscriberInterface {
           'class' => $classes,
           'title' => 'Vai a tutte le novità',
         ];
-        $view_novita = views_embed_view('taxonomy_term', 'block_novita', $term->id());
-        if ($view_novita) {
-          $build['novita']['search_area'] = array(
-            '#theme' => 'search_area',
-            '#theme_wrappers' => ['search_area'],
-            '#view' => $view_novita,
-            '#title' => 'Novità',
-            '#id' => 'novita',
-            '#classes' => ['neutral-2-bg', 'pt-lg-6'],
-            '#link' => $link,
-          );
+
+        $display_id = 'block_novita';
+        if (!$this->checkResults($view_name, $display_id, $term->id())) {
+          $view_novita = views_embed_view($view_name, $display_id, $term->id());
+          if ($view_novita) {
+            $build['novita']['search_area'] = array(
+              '#theme' => 'search_area',
+              '#theme_wrappers' => ['search_area'],
+              '#view' => $view_novita,
+              '#title' => 'Novità',
+              '#id' => 'novita',
+              '#classes' => ['neutral-2-bg', 'pt-lg-6'],
+              '#link' => $link,
+            );
+          }
         }
+
         # Amministrazione
         $url = Url::fromUserInput('/amministrazione');
         $link = Link::fromTextAndUrl(t('Tutta l\'amministrazione'), $url)->toRenderable();
@@ -118,18 +128,23 @@ class TermPageBuildSubscriber implements EventSubscriberInterface {
           'class' => $classes,
           'title' => 'Vai a Amministrazione',
         ];
-        $view_admin = views_embed_view('taxonomy_term', 'block_amministrazione', $term->id());
-        if ($view_admin) {
-          $build['amministrazione']['search_area'] = array(
-            '#theme' => 'search_area',
-            '#theme_wrappers' => ['search_area'],
-            '#view' => $view_admin,
-            '#title' => 'Amministrazione',
-            '#id' => 'amministrazione',
-            '#classes' => [],
-            '#link' => $link,
-          );
+        $view_name = 'taxonomy_term';
+        $display_id = 'block_amministrazione';
+        if (!$this->checkResults($view_name, $display_id, $term->id())) {
+          $view_admin = views_embed_view($view_name, $display_id, $term->id());
+          if ($view_admin) {
+            $build['amministrazione']['search_area'] = array(
+              '#theme' => 'search_area',
+              '#theme_wrappers' => ['search_area'],
+              '#view' => $view_admin,
+              '#title' => 'Amministrazione',
+              '#id' => 'amministrazione',
+              '#classes' => [],
+              '#link' => $link,
+            );
+          }
         }
+
         # Servizi
         $url = Url::fromUserInput('/servizi');
         $link = Link::fromTextAndUrl(t('Tutti i servizi'), $url)->toRenderable();
@@ -137,37 +152,47 @@ class TermPageBuildSubscriber implements EventSubscriberInterface {
           'class' => $classes,
           'title' => 'Vai a tutti i servizi',
         ];
-        $view_services = views_embed_view('taxonomy_term', 'block_servizi', $term->id());
-        if ($view_services) {
-          $build['servizi']['search_area'] = array(
-            '#theme' => 'search_area',
-            '#theme_wrappers' => ['search_area'],
-            '#view' => $view_services,
-            '#title' => 'Servizi',
-            '#id' => 'servizi',
-            '#classes' => [],
-            '#link' => $link,
-          );
+        $view_name = 'taxonomy_term';
+        $display_id = 'block_servizi';
+        if (!$this->checkResults($view_name, $display_id, $term->id())) {
+          $view_services = views_embed_view($view_name, $display_id, $term->id());
+          if ($view_services) {
+            $build['servizi']['search_area'] = array(
+              '#theme' => 'search_area',
+              '#theme_wrappers' => ['search_area'],
+              '#view' => $view_services,
+              '#title' => 'Servizi',
+              '#id' => 'servizi',
+              '#classes' => [],
+              '#link' => $link,
+            );
+          }
         }
-        # Docuemnti
+
+        # Documenti
         $url = Url::fromUserInput('/amministrazione/documenti-e-dati');
         $link = Link::fromTextAndUrl(t('Tutti i documenti'), $url)->toRenderable();
         $link['#attributes'] = [
           'class' => $classes,
           'title' => 'Vai a tutti i documenti',
         ];
-        $view_documents = views_embed_view('taxonomy_term', 'block_documenti', $term->id());
-        if ($view_documents) {
-          $build['documenti']['search_area'] = array(
-            '#theme' => 'search_area',
-            '#theme_wrappers' => ['search_area'],
-            '#view' => $view_documents,
-            '#title' => 'Documenti',
-            '#id' => 'documenti',
-            '#classes' => [],
-            '#link' => $link,
-          );
+        $view_name = 'taxonomy_term';
+        $display_id = 'block_documenti';
+        if (!$this->checkResults($view_name, $display_id, $term->id())) {
+          $view_documents = views_embed_view($view_name, $display_id, $term->id());
+          if ($view_documents) {
+            $build['documenti']['search_area'] = array(
+              '#theme' => 'search_area',
+              '#theme_wrappers' => ['search_area'],
+              '#view' => $view_documents,
+              '#title' => 'Documenti',
+              '#id' => 'documenti',
+              '#classes' => [],
+              '#link' => $link,
+            );
+          }
         }
+
         break;
     }
 
@@ -175,4 +200,19 @@ class TermPageBuildSubscriber implements EventSubscriberInterface {
     $event->setBuildArray($build);
   }
 
+  /**
+   * Check view results
+   *
+   * @param string $view_name
+   * @param string $display_id
+   * @param int $argument
+   * @return bool
+   */
+  private function checkResults(string $view_name, string $display_id, int $argument): bool {
+    $view = Views::getView($view_name);
+    $view->setDisplay($display_id);
+    $view->setArguments([$argument]);
+    $view->execute();
+    return empty($view->result);
+  }
 }
