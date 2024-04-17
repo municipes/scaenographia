d2326b7bd8885aa3a7009e1a55de0ce52e688eb4
Maurizio Cavalletti <maurizio@akabit.it>
2024-04-17 13:22:31 +0200

    feat: mare, new color variant

--- webpack.common.js
+++ webpack.common.js
@@ -13,6 +13,7 @@ module.exports = {
     // "bootstrap-italia": [paths.src + '/js/index.js', paths.src + '/scss/theme.scss'],
     "bootstrap-italia-comuni": [paths.src + '/js/index.js', paths.src + '/scss/theme-comuni.scss'],
     "bootstrap-italia-comuni-rosso": [paths.src + '/js/index.js', paths.src + '/scss/theme-comuni-rosso.scss'],
+    "bootstrap-italia-comuni-mare": [paths.src + '/js/index.js', paths.src + '/scss/theme-comuni-mare.scss'],
     //"ckeditor5": paths.src + '/scss/ckeditor5.scss',
     "ckeditor5-comuni": paths.src + '/scss/ckeditor5-comuni.scss',
   },
